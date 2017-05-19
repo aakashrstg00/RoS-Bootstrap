893e38a2ae86ed36411394e5aa7b16a47f35828b
Aakash Rastogi <1998aakashrastogi@gmail.com>
2017-05-19 17:55:53 +0530

    Add files via upload

---
+++ js/directives/directive.js
@@ -0,0 +1,17 @@
+app.directive("rosHeader", function () {
+    return {
+        templateUrl: "../../pages/directives/header.html"
+        , restrict: "E"
+    };
+});
+app.directive("rosFooter", function () {
+    return {
+        templateUrl: "../../pages/directives/footer.html"
+        , restrict: "E"
+    };
+});
+app.filter("removeYear", function () {
+    return function (str) {
+        return str.substring(0, str.length - 5);
+    };
+});
