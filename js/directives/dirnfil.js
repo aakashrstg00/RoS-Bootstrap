0ce50faf8fbf69798fde3d09a40797571b642f7e
Aakash Rastogi <1998aakashrastogi@gmail.com>
2017-05-21 02:39:15 +0530

    Add files via upload

--- js/directives/dirnfil.js
+++ js/directives/dirnfil.js
@@ -1,12 +1,12 @@
 app.directive("rosHeader", function () {
     return {
-        templateUrl: "<nav class=\" navbar navbar-toggleable-sm navbar-inverse bg-inverse \"><div class=\"container \"> <ul class=\"nav navbar-nav \"> <li class=\"nav-item \"> <a class=\"nav-link \" href=\"#/home\" style=\"font-family:'Montserrat',sans-serif\">Home</a> </li> <li class=\"nav-item \"> <a class=\"nav-link \" href=\"#/aboutus\" style=\"font-family:'Montserrat',sans-serif\">About Us</a> </li>    </ul> <a href=\"https://github.com/aakashrstg00/RoS-Bootstrap\" style=\"font-family:'Montserrat',sans-serif\" class=\"navbar-brand ml-sm-auto mr-0\" target=\"_blank\">Raindrops or Sunshine</a> </div> </nav>"
+        template: "<nav class=\"navbar navbar-toggleable-sm navbar-inverse bg-inverse\"><div class=\"container\"><ul class=\"nav navbar-nav\"> <li class=\"nav-item\"><a class=\"nav-link \" href=\"#/home\" style=\"font-family:'Montserrat',sans-serif\">Home</a></li><li class=\"nav-item\"><a class=\"nav-link\" href=\"#/aboutus\" style=\"font-family:'Montserrat',sans-serif\">About Us</a></li></ul><a href=\"https://github.com/aakashrstg00/RoS-Bootstrap\" style=\"font-family:'Montserrat',sans-serif\" class=\"navbar-brand ml-sm-auto mr-0\" target=\"_blank\">Raindrops or Sunshine</a></div></nav>"
         , restrict: "E"
     };
 });
 app.directive("rosFooter", function () {
     return {
-        templateUrl: "<footer class=\"footer container py-3 text-white\"> <hr style=\"border-color: white\"> <div class=\"row \"> <div class=\"col-sm-7 \">&psi;<strong> Aakash Rastogi </strong>&psi;</div> <div class=\"col-sm-5 text-md-right \"> &copy; 2017 Raindrops or Sunshine</div> </div> </footer>"
+        template: "<footer class=\"footer container py-3 text-white\"><hr style=\"border-color: white\"><div class=\"row \"><div class=\"col-sm-7\">&psi;<strong> Aakash Rastogi </strong>&psi;</div><div class=\"col-sm-5 text-md-right\"> &copy; 2017 Raindrops or Sunshine</div></div></footer>"
         , restrict: "E"
     };
 });
